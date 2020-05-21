972c6281d814e2197b2e652f0b82d9a6c274199c
Sai Chandra Chilupuri <saichandrachilupuri@gmail.com>
2020-05-21 21:03:30 +0530

    Intial commit

---
+++ src/main/java/com/netent/thebookstore/ThebookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.netent.thebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netent/thebookstore/ThebookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netent.thebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
