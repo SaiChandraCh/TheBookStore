1ceb46c65a713c5b353261805901752b3f074aad
Sai Chandra Chilupuri <saichandrachilupuri@gmail.com>
2020-05-21 23:01:53 +0530

    Updating book model

--- src/main/java/com/netent/thebookstore/dao/model/Book.java
+++ src/main/java/com/netent/thebookstore/dao/model/Book.java
@@ -1,4 +1,47 @@
 package com.netent.thebookstore.dao.model;
 
 public class Book {
+    private int ISBN;
+    private String title;
+    private String author;
+    private int price;
+
+    public Book(int ISBN, String title, String author, int price) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(int ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
 }
